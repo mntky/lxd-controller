ec6d26dc6bb92d0e5d33f928d15957b6c5c5d3df
mntky <djminatoo@gmail.com>
2019-04-15 14:56:05 +0900

    [add] container action

--- pkg/lxdapi.go
+++ pkg/lxdapi.go
@@ -25,3 +25,81 @@ func Status(name string, container lxd.ContainerServer) *api.ContainerState {
 	fmt.Println(str)
 	return stat
 }
+
+func Create(name string, container lxd.ContainerServer) (string, error) {
+	resp := "[Complete] create " + name
+	req := api.ContainersPost{
+		Name: name,
+		Source: api.ContainerSource{
+			Type: "image",
+			Alias: "debian",
+		},
+	}
+
+	op, err := container.CreateContainer(req)
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	err = op.Wait()
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	return resp, err
+}
+
+func Delete(name string, container lxd.ContainerServer) (string, error) {
+	resp := "[Complete] delete" + name
+	op, err := container.DeleteContainer(name)
+	if err != nil{
+		resp = "[Failed]"
+		return resp, err
+	}
+	err = op.Wait()
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	return resp, err
+}
+
+func Start(name string, container lxd.ContainerServer) (string, error) {
+	resp := "[Complete] start" + name
+	reqState := api.ContainerStatePut {
+		Action: "start",
+		Timeout: -1,
+	}
+
+	op, err := container.UpdateContainerState(name, reqState, "")
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	err = op.Wait()
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	return resp, err
+}
+
+func Stop(name string, container lxd.ContainerServer) (string, error) {
+	resp := "[Complete] stop" + name
+	reqState := api.ContainerStatePut {
+		Action: "stop",
+		Timeout: -1,
+	}
+
+	op, err := container.UpdateContainerState(name, reqState, "")
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	err = op.Wait()
+	if err != nil {
+		resp = "[Failed]"
+		return resp, err
+	}
+	return resp, err
+}
